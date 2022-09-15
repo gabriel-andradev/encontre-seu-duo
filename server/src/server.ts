8bbddad08c74ac8fd339f9ffd886ded918f7931f
Gabriel Andrade <gandrade.dev@gmail.com>
2022-09-14 22:17:41 -0300

    Create database table Game and Ads, routes /games

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,31 @@
-import express, { response } from "express";
+import express from 'express';
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
-
-app.get('/games', (request, response) => {
-  return response.json()
+const prisma = new PrismaClient({
+  log: ['query']
 })
+
+
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany()
+
+  return response.json(games)
+});
 
 app.get('/ads', (request, response) => {
   return response.status(201).json([])
 });
 
-app.get('/games/:id/discord', (request, response) => {
-
-  return response.json([])
-})
+app.get('/games/:id/ads', (request, response) => {
+  return response.json([
+    { id: 1, name: "Anúncio 1" },
+    { id: 2, name: "Anúncio 2" },
+    { id: 3, name: "Anúncio 3" },
+    { id: 4, name: "Anúncio 4" },
+    { id: 5, name: "Anúncio 5" },
+    { id: 6, name: "Anúncio 6" },
+  ])
+});
 
 app.listen(3333)
